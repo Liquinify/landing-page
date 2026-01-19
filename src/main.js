ccd4b7e7a2f3f62e6c49b3528f855416c38a9e46
liquinify <a.amin-afshar@globaltravel.space>
2026-01-19 20:55:43 +0300

    feat: input validation

--- src/main.js
+++ src/main.js
@@ -5,8 +5,51 @@ const pcCarouselButtons = document.querySelector('.carousel-buttons');
 const pcCarouselCounter = document.querySelector('.carousel-counter');
 const faqAnswers = document.querySelectorAll('.faq-answer');
 const faqButtons = document.querySelectorAll('.faq-toggle');
+const contactForm = document.querySelector('.contact-form');
+const nameInput = document.querySelector('.contact-name');
+const emailInput = document.querySelector('.contact-email');
+const messageInput = document.querySelector('.contact-message');
+
 
 document.addEventListener('DOMContentLoaded', () => {
+    contactForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const existingError = document.querySelector('.error-message');
+      if (existingError) {
+        existingError.remove();
+      }
+
+      nameInput.classList.remove('input-error');
+      emailInput.classList.remove('input-error');
+      messageInput.classList.remove('input-error');
+
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      
+      if (!emailInput.value.trim() || !emailRegex.test(emailInput.value.trim())) {
+        emailInput.classList.add('input-error');
+        const errorP = document.createElement('p');
+        errorP.className = 'error-message';
+        errorP.textContent = 'Incorrect email type';
+        emailInput.insertAdjacentElement('afterend', errorP);
+      } 
+
+      if(!nameInput.value.trim() || !nameInput.value.length){ 
+        nameInput.classList.add('input-error');
+        const errorP = document.createElement('p');
+        errorP.className = 'error-message';
+        errorP.textContent = 'Name cannot be empty';
+        nameInput.insertAdjacentElement('afterend', errorP);
+      }
+
+      if(!messageInput.value.trim() || !messageInput.value.length){ 
+        messageInput.classList.add('input-error');
+        const errorP = document.createElement('p');
+        errorP.className = 'error-message';
+        errorP.textContent = 'Message cannot be empty';
+        messageInput.insertAdjacentElement('afterend', errorP);
+      }
+    });
+  
   faqButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
       const answer = faqAnswers[index];
